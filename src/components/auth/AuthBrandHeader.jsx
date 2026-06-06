34bda66bdab4a0ab4faeff486d7da3dc71f7bb16
soybarusa-lgtm <soybarusa@gmail.com>
2026-06-06 12:45:35 +0200

    Use negative logo in auth header

--- src/components/auth/AuthBrandHeader.jsx
+++ src/components/auth/AuthBrandHeader.jsx
@@ -1,18 +1,11 @@
-export default function AuthBrandHeader({ eyebrow = 'farmacias vivas' }) {
+export default function AuthBrandHeader() {
   return (
-    <div className="text-center">
-      <div className="flex justify-center">
-        <img
-          src="/brand/logo-full-color.svg"
-          alt="Viteka"
-          className="h-14 w-auto max-w-[190px] object-contain"
-        />
-      </div>
-      {eyebrow ? (
-        <p className="mt-3 text-[0.7rem] font-semibold uppercase tracking-[0.32em] text-white/75">
-          {eyebrow}
-        </p>
-      ) : null}
+    <div className="flex justify-center">
+      <img
+        src="/brand/logo-white.svg"
+        alt="Viteka"
+        className="h-16 w-auto max-w-[220px] object-contain"
+      />
     </div>
   )
 }
